e45fa774e3d3e191793f4e122556df82df2c61fd
Reginaldo Morikawa <morikawa77@gmail.com>
2021-06-01 20:29:48 -0300

    Exercício final UC09

---
+++ exercicioFinalUC09/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace exercicioFinalUC09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, resposta = "";
+            double raio, circunferencia;
+
+            Console.WriteLine("Digite o seu nome:");
+            nome = Console.ReadLine();
+
+            Console.WriteLine($"Olá {nome}! Você é terraplanista?");
+            resposta = Console.ReadLine().ToLower();
+
+            while(resposta != "sim" && resposta != "não" && resposta != "nao"){
+                Console.WriteLine("Resposta inválida. Tente novamente.");
+                Console.WriteLine($"Olá {nome}! Você é terraplanista?");
+                resposta = Console.ReadLine().ToLower();
+            }
+
+            if(resposta == "sim") {
+                Environment.Exit(0);
+            } else if(resposta == "não" || resposta == "nao") {
+                Console.WriteLine("Digite o raio da Terra:");
+                raio = double.Parse(Console.ReadLine());
+
+                circunferencia = 2 * Math.PI * raio;
+                Console.WriteLine($"A circunferência da Terra é: {circunferencia}");
+                Environment.Exit(0);
+            } 
+
+        }
+    }
+}
