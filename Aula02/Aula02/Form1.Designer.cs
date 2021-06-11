63aa1f9befa44ecb9da6ac71f2acca70e5d9e406
Reginaldo Morikawa <morikawa77@gmail.com>
2021-06-10 21:27:36 -0300

    First Windows Form App

---
+++ Aula02/Aula02/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿
+namespace Aula02
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.lbl_Descricao = new System.Windows.Forms.Label();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.txtNomeOK = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(47, 64);
+            this.txtNome.Multiline = true;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(378, 48);
+            this.txtNome.TabIndex = 0;
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirmar.Image = ((System.Drawing.Image)(resources.GetObject("btnConfirmar.Image")));
+            this.btnConfirmar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnConfirmar.Location = new System.Drawing.Point(266, 317);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.btnConfirmar.Size = new System.Drawing.Size(159, 43);
+            this.btnConfirmar.TabIndex = 1;
+            this.btnConfirmar.Text = "OK";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // lbl_Descricao
+            // 
+            this.lbl_Descricao.AutoSize = true;
+            this.lbl_Descricao.Location = new System.Drawing.Point(44, 39);
+            this.lbl_Descricao.Name = "lbl_Descricao";
+            this.lbl_Descricao.Size = new System.Drawing.Size(38, 13);
+            this.lbl_Descricao.TabIndex = 2;
+            this.lbl_Descricao.Text = "Nome:";
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold);
+            this.btnSair.Image = ((System.Drawing.Image)(resources.GetObject("btnSair.Image")));
+            this.btnSair.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSair.Location = new System.Drawing.Point(47, 317);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.btnSair.Size = new System.Drawing.Size(145, 43);
+            this.btnSair.TabIndex = 3;
+            this.btnSair.Text = "SAIR";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // txtNomeOK
+            // 
+            this.txtNomeOK.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNomeOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNomeOK.Location = new System.Drawing.Point(47, 196);
+            this.txtNomeOK.Multiline = true;
+            this.txtNomeOK.Name = "txtNomeOK";
+            this.txtNomeOK.Size = new System.Drawing.Size(378, 48);
+            this.txtNomeOK.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 168);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nome OK:";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 387);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNomeOK);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.lbl_Descricao);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.txtNome);
+            this.Name = "frmPrincipal";
+            this.Text = "Formulario Principal";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Label lbl_Descricao;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.TextBox txtNomeOK;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Aula02/Aula02/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula02
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            txtNomeOK.Text = txtNome.Text;
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
